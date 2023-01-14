06282065088a39e3f51a57c08bee26e5d78e8445
Shigma <shigma10826@gmail.com>
2023-01-15 00:05:05 +0800

    feat: add basic implementation

---
+++ src/index.ts
@@ -0,0 +1,82 @@
+import { defineComponent, h } from 'vue'
+import { marked } from 'marked'
+import xss from 'xss'
+
+// Sections derived from MDN element categories and limited to the more
+// benign categories.
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element
+const  allowedTags = [
+  // Content sectioning
+  'address', 'article', 'aside', 'footer', 'header',
+  'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hgroup',
+  'main', 'nav', 'section',
+  // Text content
+  'blockquote', 'dd', 'div', 'dl', 'dt', 'figcaption', 'figure',
+  'hr', 'li', 'main', 'ol', 'p', 'pre', 'ul',
+  // Inline text semantics
+  'a', 'abbr', 'b', 'bdi', 'bdo', 'br', 'cite', 'code', 'data', 'dfn',
+  'em', 'i', 'kbd', 'mark', 'q',
+  'rb', 'rp', 'rt', 'rtc', 'ruby',
+  's', 'samp', 'small', 'span', 'strong', 'sub', 'sup', 'time', 'u', 'var', 'wbr',
+  // Table content
+  'caption', 'col', 'colgroup', 'table', 'tbody', 'td', 'tfoot', 'th',
+  'thead', 'tr',
+]
+
+const voidTags = ['img', 'br', 'hr', 'area', 'base', 'basefont', 'input', 'link', 'meta']
+
+function sanitize(html: string) {
+  const whiteList: XSS.IWhiteList = {
+    ...Object.fromEntries(allowedTags.map(tag => [tag, []])),
+    a: ['target', 'href', 'title', 'rel'],
+  }
+  const stack: string[] = []
+  html = xss(html, {
+    whiteList,
+    onTag(tag, html, options) {
+      if (html.endsWith('/>') || voidTags.includes(tag)) {
+        return html
+      }
+      if (!options.isClosing) {
+        stack.push(tag)
+        return html
+      }
+      let result = ''
+      while (stack.length) {
+        const last = stack.pop()
+        if (last === tag) {
+          return result + html
+        }
+        result += `</${last}>`
+      }
+      return html.replace(/</g, '&lt;').replace(/>/g, '&gt;')
+    },
+  })
+  while (stack.length) {
+    const last = stack.pop()
+    html += `</${last}>`
+  }
+  return html
+}
+
+export default defineComponent({
+  props: {
+    source: String,
+    inline: Boolean,
+    tag: String,
+    safe: Boolean,
+  },
+  setup(props) {
+    let html = props.inline
+      ? marked.parseInline(props.source || '')
+      : marked.parse(props.source || '')
+    if (props.safe ?? props.inline) html = sanitize(html)
+    return () => {
+      const tag = props.tag || (props.inline ? 'span' : 'div')
+      return h(tag, {
+        class: 'markdown',
+        innerHTML: html,
+      })
+    }
+  },
+})
